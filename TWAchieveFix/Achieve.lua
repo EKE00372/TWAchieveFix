82cb11f1b68837374d79b9583a415b4478a56d02
EK <eke00372@gmail.com>
2023-01-29 07:08:21 +0800

    Add files via upload

--- TWAchieveFix/Achieve.lua
+++ TWAchieveFix/Achieve.lua
@@ -1,16 +1,19 @@
--- force load
 LoadAddOn("Blizzard_AchievementUI")
 
--- copy origin code
-function AchievementButton_Localize(button)
-	--button.DateCompleted:SetWidth(80);
-	--button.DateCompleted:SetPoint("TOP", "$parent", "BOTTOM", -2, 6);
-	--button.DateCompleted:SetJustifyH("CENTER");
-	
-	-- use correct font object with shadow offset
+local tw = C_CVar.GetCVar("textLocale")
+if tw ~= zhTW then return end
+
+local function AchievementButton_Localize_Fix(button)
 	button.Description:SetFontObject("GameFontHighlight")
 	button.HiddenDescription:SetFontObject("GameFontHighlight")
-	
-	--button.Shield:SetPoint("TOPRIGHT", -10, 0);
-	--button.Tabard:SetPoint("TOPRIGHT", -7, -4);
+	button.DateCompleted:SetPoint("TOP", button.Shield, "BOTTOM", -2, 6)
 end
+
+hooksecurefunc("AchievementButton_Localize", AchievementButton_Localize_Fix)
+
+
+local function AchievementFrame_LocalizeCriteria_Fix(frame)
+	frame.Name:SetFontObject("GameFontHighlight")
+end
+
+hooksecurefunc("AchievementFrame_LocalizeCriteria", AchievementFrame_LocalizeCriteria_Fix)
