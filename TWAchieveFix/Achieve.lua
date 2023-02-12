9cfc788ad540723d2ec5b3f72863175651b671eb
EK <eke00372@gmail.com>
2023-02-12 17:06:04 +0800

    fix load issue

--- TWAchieveFix/Achieve.lua
+++ TWAchieveFix/Achieve.lua
@@ -1,15 +1,22 @@
-LoadAddOn("Blizzard_AchievementUI")
-
 local tw = C_CVar.GetCVar("textLocale")
 if tw ~= "zhTW" then return end
 
+local function AchievementFont_Small_Fix()
+	AchievementFont_Small:SetShadowColor(0, 0, 0)
+	AchievementFont_Small:SetShadowOffset(1, -1)
+end
+
 local function AchievementButton_Localize_Fix(button)
-	button.Description:SetFontObject("GameFontHighlight")
-	button.HiddenDescription:SetFontObject("GameFontHighlight")
 	button.DateCompleted:SetPoint("TOP", button.Shield, "BOTTOM", -2, 6)
 end
-hooksecurefunc("AchievementButton_Localize", AchievementButton_Localize_Fix)
 
-function AchievementFrame_LocalizeCriteria (frame)
-	frame.Name:SetFontObject("GameFontHighlight")
-end
+local frame = CreateFrame("FRAME")
+	frame:RegisterEvent("ADDON_LOADED")
+		frame:SetScript("OnEvent", function(self, event, addon)
+		if event == "ADDON_LOADED" then
+			if addon == "Blizzard_AchievementUI" then
+				AchievementFont_Small_Fix()
+				hooksecurefunc("AchievementButton_Localize", AchievementButton_Localize_Fix)
+			end
+		end
+	end)
