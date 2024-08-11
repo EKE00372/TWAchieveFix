cba582eb067ab1c0f8f5ed95de3b4dfbca8022f1
EK <eke00372@gmail.com>
2024-08-11 13:49:07 +0800

    TWW update

--- TWAchieveFix/Achieve.lua
+++ TWAchieveFix/Achieve.lua
@@ -6,17 +6,12 @@ local function AchievementFont_Small_Fix()
 	AchievementFont_Small:SetShadowOffset(1, -1)
 end
 
-local function AchievementButton_Localize_Fix(button)
-	button.DateCompleted:SetPoint("TOP", button.Shield, "BOTTOM", -2, 6)
-end
-
 local frame = CreateFrame("FRAME")
 	frame:RegisterEvent("ADDON_LOADED")
 		frame:SetScript("OnEvent", function(self, event, addon)
 		if event == "ADDON_LOADED" then
 			if addon == "Blizzard_AchievementUI" then
 				AchievementFont_Small_Fix()
-				hooksecurefunc("AchievementButton_Localize", AchievementButton_Localize_Fix)
 			end
 		end
 	end)
